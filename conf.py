bca32962082401e68c8deba74d06cfe95e392db6
Matthew Brett <matthew.brett@gmail.com>
2017-02-13 16:52:34 +0000

    RF: adapt latex options to modern Sphinx
    Sphinx >= 0.5 prefers the paper size, font size and preamble in a
    ``latex_elements`` dictionary.

--- conf.py
+++ conf.py
@@ -170,13 +170,25 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'curious_git'
 
-# -- Options for LaTeX output --------------------------------------------------
+# -- Options for LaTeX output ---------------------------------------------
 
-# The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-latex_elements = {'font_size': '11pt'}
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass
